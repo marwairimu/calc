fb842ee75e83055cd8b23407a3f5a494ab9599cb
marwairimu <92425844+marwairimu@users.noreply.github.com>
2021-10-13 13:36:12 +0300

    Add files via upload

---
+++ calculator.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+	int sum,numm1,numm2,diff,product,quotient;
+	printf("Enter first numbers:" );
+	scanf("%d %d ",&numm1, &numm2);
+	
+	sum=numm1+numm2;
+	diff=numm1-numm2;
+	product=numm1*numm2;
+	quotient = (float) numm1/numm2;
+
+	printf("%d + %d = %d", numm1, numm2, sum);
+	printf("%d - %d = %d", numm1, numm2, diff);
+	printf("%d * %d = %d", numm1, numm2, product);
+	printf("%d / %d = %d", numm1, numm2, quotient);
+	
+	return 0;
+}
